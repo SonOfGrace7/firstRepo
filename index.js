e927aa7d5f45533b8b737a49659d4b78c8cde1fc
SonOfGrace7 <ernestjboy29@gmail.com>
2024-11-13 03:07:46 -0800

    more on script

---
+++ index.js
@@ -0,0 +1,65 @@
+intro = "Hello world II ";
+console.log("HELLO WORLD II");
+// alert("Hello world II");
+// document.write("Hello world II dSOUOUNAVOUNOA");
+// document.getElementById("second").innerHTML = "Hello World II jhoufhciuc";
+
+
+
+myName = "Kay kay xp";
+myName = "kamsi ugwu";
+
+const mylikes = " be very wealthy and healthy";
+
+let shop = [
+    "shirts",
+    "trousers",
+    ["polo", "baggi", "shorts"],
+    "inners",
+    "babbies",
+];
+
+console.log(shop[2][0])
+
+
+let fruits =["Banana", "Orange", "Apple","Mango"];
+
+
+fruits.push("cashew");
+fruits.push(`cova`);
+// fruits.pop();
+// fruits.pop();
+
+// fruits.shift();
+fruits.unshift("pawpaw");
+delete fruits[0];
+
+
+let arrJoin = fruits;
+
+console.log(arrJoin);
+
+const myGirls =["joy", "lone"];
+const myBoys = ["Emil","Tobias" ,"linus"];
+let myKids = myBoys.concat(myGirls, fruits);
+myKids = myKids.join("... ");
+console.log(myBoys);
+fruits.copyWithin(2, 1);
+console.log(myKids);
+
+
+
+const myArr = [
+    [1, 2],
+    [3, 4],
+    [5, 6],
+];
+const newArr = myArr.flat();
+console.log(newArr);
+
+
+fruits = ["Banana", "Orange", "Apple","Mangonn"];
+let myfruit =fruits.slice(1, 4 );
+console.log(fruits);
+console.log(myfruit)
+
