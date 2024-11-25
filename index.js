1f83e92f8ced82c31086389a27a3744f59e0e37a
SonOfGrace7 <ernestjboy29@gmail.com>
2024-11-25 23:40:32 +0100

    first commit

--- index.js
+++ index.js
@@ -4,26 +4,22 @@ console.log("HELLO WORLD II");
 // document.write("Hello world II dSOUOUNAVOUNOA");
 // document.getElementById("second").innerHTML = "Hello World II jhoufhciuc";
 
-
-
 myName = "Kay kay xp";
 myName = "kamsi ugwu";
 
 const mylikes = " be very wealthy and healthy";
 
 let shop = [
-    "shirts",
-    "trousers",
-    ["polo", "baggi", "shorts"],
-    "inners",
-    "babbies",
+  "shirts",
+  "trousers",
+  ["polo", "baggi", "shorts"],
+  "inners",
+  "babbies",
 ];
 
-console.log(shop[2][0])
+console.log(shop[2][0]);
 
-
-let fruits =["Banana", "Orange", "Apple","Mango"];
-
+let fruits = ["Banana", "Orange", "Apple", "Mango"];
 
 fruits.push("cashew");
 fruits.push(`cova`);
@@ -34,63 +30,59 @@ fruits.push(`cova`);
 fruits.unshift("pawpaw");
 delete fruits[0];
 
-
 let arrJoin = fruits;
 
 console.log(arrJoin);
 
-const myGirls =["joy", "lone"];
-const myBoys = ["Emil","Tobias" ,"linus"];
+const myGirls = ["joy", "lone"];
+const myBoys = ["Emil", "Tobias", "linus"];
 let myKids = myBoys.concat(myGirls, fruits);
 myKids = myKids.join("... ");
 console.log(myBoys);
 fruits.copyWithin(2, 1);
 console.log(myKids);
 
-
-
-// c//onst myArr = [
+// const myArr = [
 //     [1, 2],
 //     [3, 4],
 //     [5, 6],
 // //];
-// c//onst newArr = myArr.flat();
+// const newArr = myArr.flat();
 // //console.log(newArr);
 
-
-fruits = ["Banana", "Orange", "Apple","Mangonn"];
-let myfruit =fruits.slice(1, 4 );
+fruits = ["Banana", "Orange", "Apple", "Mangonn"];
+let myfruit = fruits.slice(1, 4);
 console.log(fruits);
-console.log(myfruit)
+console.log(myfruit);
 
 // fruits.sort()
 //console.log(fruits)
 
 let sorted = fruits.toSorted();
-console.log(sorted)
+console.log(sorted);
 
 const points = [40, 100, 1, 5, 25, 10];
-// points.sort((a, b) => a - b); for ascending order call back 
+// points.sort((a, b) => a - b); for ascending order call back
 // points.sort((a, b) => b - a); for descending order
 points.sort((a, b) => a - b);
 console.log(points.at(-2));
 console.log(Math.max.apply(null, points));
 
 cars = [
-    { type: "Volvo", year: 2016},
-    { type: "Saab", year: 2001},
-    { type: "BNW", year: 2010},
+  { type: "Volvo", year: 2016 },
+  { type: "Saab", year: 2001 },
+  { type: "BNW", year: 2010 },
 ];
- 
+
 cars.sort((a, b) => a - b);
 console.log(cars);
 
- let changed = []
+let changed = [];
 points.forEach((x, inx, arr) => {
-    changed.push((x + 20));
+  changed.push(x + 20);
 });
 
-// console.log(changed); 
+// console.log(changed);
 
 // points.map((x, idx, arr) => {
 //    changed.push((x += idx));
@@ -112,7 +104,6 @@ let bool = point1.some((value) => value > 50);
 
 console.log(bool);
 
-
 let strings = "ada, obi, nneka, ekene";
 
 let names = Array.from(strings);
@@ -128,3 +119,40 @@ const q4 = ["Oct", "Nov", "May"];
 const year = [...q1, ...q2, ...q3, ...q4];
 console.log([year]);
 console.log([...q1, "ap", "may"]);
+
+// JAVASCRIPT DATE
+let myDate = new Date();
+
+let myDay = [
+  "sunday",
+  "monday",
+  "tuesday",
+  "wednesday",
+  "thursday",
+  "friday",
+  "saturday",
+];
+
+console.log(myDays[myDate.getDay()]);
+
+let myMonths = [
+  "sunday",
+  "monday",
+  "tuesday",
+  "wednesday",
+  "thursday",
+  "friday",
+  "saturday",
+];
+
+let month = myDate.getMonth();
+console.log(myMonths[month]);
+
+let currentDate = new Date();
+let after = currentDate.setFullYear("2020");
+
+currentDate.setDate(10);
+console.log(new Date(currentDate));
+
+let currentDate1 = new Date();
+let after1 = currentDate.setFullYear("2020");
